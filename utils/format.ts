dacafc0a39fa8ace08d079f00c76e3134ae8d29e
Phạm Văn Minh Nhựt <phamvmnhut@gmail.com>
2022-10-20 16:05:05 +0700

    marketplace filter

--- utils/format.ts
+++ utils/format.ts
@@ -8,3 +8,8 @@ export function format_number_2_digit(num: number) {
 export function truncate(str: string, n: number) {
     return (str.length > n) ? str.slice(0, n - 1) + '...' : str;
 }
+
+export function parseIntT(str: any) : number {
+    let strS = !str ? "0" : str;
+    return parseInt(strS, 10);
+}
